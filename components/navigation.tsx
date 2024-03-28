6316f5a6a6ab9aeb730b7f38b128fe9fa338e847
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-03-29 02:28:16 +0900

    add to Daehungod

---
+++ components/navigation.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+
+export default function Navigation() {
+  const path = usePathname();
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link> {path === "/" ? "🍎" : ""}
+        </li>
+        <li>
+          <Link href="/about-us">About Us</Link>{" "}
+          {path === "/about-us" ? "⭐️" : ""}
+        </li>
+      </ul>
+    </nav>
+  );
+}
